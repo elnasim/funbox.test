d03275ecba7552afc5760d406dc16104fc233d58
Serega KTyH <archlis21@gmail.com>
2020-08-28 18:22:38 +0300

    cards add dynamic

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.scss";
 
-export default function Card() {
+export default function Card({ data }) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const selectHandler = () => {
+    setIsSelected(!isSelected);
+  };
+
+  const bottomText = () => {
+    if (!data.available) {
+      return {
+        __html: data.cardBottomText.notAvalible,
+      };
+    }
+    if (isSelected) {
+      return {
+        __html: data.cardBottomText.selected,
+      };
+    }
+    if (!isSelected) {
+      return {
+        __html: data.cardBottomText.default,
+      };
+    }
+  };
+
+  let cardStyles = ["card"];
+
+  if (!data.available) {
+    cardStyles.push("disabled");
+  }
+
+  if (isSelected) {
+    cardStyles.push("selected");
+  }
+
   return (
     <div className="card-col">
-      <div className="card">
+      <a href="#" className={cardStyles.join(" ")} onClick={selectHandler}>
         <div className="card__inner">
-          <div className="card__toptext">Сказочное заморское яство</div>
-          <div className="card__title">Нямушка</div>
-          <div className="card__subtitle">с фуа-гра</div>
-          <div className="card__thin-text">10 порций</div>
-          <div className="card__thin-text">мышь в подарок</div>
+          <div className="card__toptext">{data.topline}</div>
+          <div className="card__title">{data.title}</div>
+          <div className="card__subtitle">{data.subtitle}</div>
+          {data.subtext.map((item) => (
+            <div className="card__thin-text">{item}</div>
+          ))}
           <div className="round-card card__round">
-            <div className="round-card__num">2</div>
+            <div className="round-card__num">{data.weight}</div>
             <div className="round-card__text">кг</div>
           </div>
         </div>
-      </div>
+      </a>
 
-      <div className="card-botline">
-        Чего сидишь? Порадуй котэ, <a href="#">купи</a>.
-      </div>
+      <div
+        className="card-botline"
+        dangerouslySetInnerHTML={bottomText()}
+      ></div>
     </div>
   );
 }
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainContent.scss";
 import Card from "../Card/Card";
 
 export default function MainContent() {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      topline: "Сказочное заморское яство",
+      title: "Нямушка",
+      subtitle: "с фуа-гра",
+      subtext: ["10 порций", "мышь в подарок"],
+      weight: "0,5",
+      cardBottomText: {
+        default: 'Чего сидишь? Порадуй котэ, <a href="#">купи</a>',
+        selected: "Печень утки разварная с артишоками.",
+        notAvalible: "Печалька, с фуа-гра закончился.",
+      },
+      available: true,
+    },
+    {
+      id: 2,
+      topline: "Сказочное заморское яство",
+      title: "Нямушка",
+      subtitle: "с рыбой",
+      subtext: ["40 порций", "2 мыши в подарок"],
+      weight: 2,
+      cardBottomText: {
+        default: 'Чего сидишь? Порадуй котэ, <a href="#">купи</a>',
+        selected: "Печень утки разварная с артишоками.",
+        notAvalible: "Печалька, с фуа-гра закончился.",
+      },
+      available: true,
+    },
+    {
+      id: 3,
+      topline: "Сказочное заморское яство",
+      title: "Нямушка",
+      subtitle: "с курой",
+      subtext: ["100 порций", "5 мышей в подарок", "заказчик доволен"],
+      weight: 5,
+      cardBottomText: {
+        default: 'Чего сидишь? Порадуй котэ, <a href="#">купи</a>',
+        selected: "Печень утки разварная с артишоками.",
+        notAvalible: "Печалька, с фуа-гра закончился.",
+      },
+      available: false,
+    },
+  ]);
+
   return (
     <div className="main-content">
       <div className="container">
         <div className="title">Ты сегодня покормил кота?</div>
 
         <div className="cards row">
-          <Card />
-          <Card />
-          <Card />
+          {data.map((item) => (
+            <Card key={item.id} data={item} />
+          ))}
         </div>
       </div>
     </div>
